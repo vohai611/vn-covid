6a2e1eab448ebc6a55dc8b277839d8d57ca5dbe2
Haivo <hai835559@gmail.com>
2021-09-30 20:33:27 +0200

    cache data for faster load

---
+++ vn-covid/load-and-cache.R
@@ -0,0 +1,58 @@
+# Load and cache necessary data
+
+library(shiny)
+library(tidyverse)
+library(plotly)
+library(here)
+library(rvest)
+library(DT)
+library(scales)
+library(googlesheets4)
+
+i_am("vn-covid/server.R")
+# load vnjson map for echarts
+small_vnjson <- read_rds(here('vn-covid/small-vnjson.rds'))
+# id to googlesheet
+ss <- "1NL6ikAYrvB2law5ZqdF3gTURMfONIgxdDbu88bvr36k"
+# Load data ---------------------------------------------------------------------------------------------
+
+# read and cache
+
+data <- read_rds(here("vn-covid/cache_data.rds"))
+
+if (data$date != Sys.Date()) {
+  main_plot <- read_sheet(ss, sheet = "main-plot")
+  case_in_com <- read_sheet(ss, "case_in_community") %>%
+    filter(name %in% c('blockade', 'community')) %>%
+    mutate(date = lubridate::date(date))
+  
+  # Load total cases/ death
+  df <-
+    read_html("https://static.pipezero.com/covid/data.json") |>
+    html_text() |>
+    jsonlite::fromJSON()
+  
+  df <- df$locations |>
+    as_tibble() |>
+    mutate(
+      name = stringi::stri_trans_general(name, id = "Latin - ASCII"),
+      name = if_else(name == "TP. Ho Chi Minh", "Ho Chi Minh", name)
+    ) |>
+    select(name, death, cases)
+  
+  date <- Sys.Date()
+  
+  cache_data <- lst(main_plot,case_in_com, df, date)
+  write_rds(cache_data, here("vn-covid/cache_data.rds"))
+} else {
+  main_plot <- data$main_plot
+  case_in_com <- data$case_in_com
+  df <- data$df
+}
+
+# Load population
+population <- read_rds(here('vn-covid/danso.rds'))
+population <- population %>% 
+  mutate(tinh_thanh = if_else(tinh_thanh == 'TP HCM', 'Ho Chi Minh', tinh_thanh))
+# load medical_stats
+medical_stat <- read_rds(here("vn-covid/medical-stat.rds"))
